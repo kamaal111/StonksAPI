454f0916993888387b0b0383d14a3fca0f3a41cd
kamaal111 <kamaal.f1@gmail.com>
2021-05-03 19:56:27 +0200

    Splitting up endpoints

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 from typing import Optional
 from fastapi import FastAPI
 from datetime import date, timedelta, datetime
+from time import perf_counter
 import yfinance
 
 
 SUPPORTED_INTERVALS = ["1m", "2m", "5m", "15m", "30m", "60m", 
                         "90m", "1h", "1d", "5d", "1wk", "1mo", "3mo"]
 
+
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/quote/{quote}")
-def get_quote_data(quote: str, interval: str = "1d", start_date: str = ""):
+@app.get("/info/{symbol}")
+def get_info(symbol: str):
+    response = {}
+    for (ticker_key, ticker) in yfinance.Tickers(symbol).tickers.items():
+        info = ticker.info
+        response_info = {}
+        response_info["logo_url"] = info["logo_url"]
+        response_info["previous_close"] = info["previousClose"]
+        response_info["short_name"] = info["shortName"]
+        response_info["long_name"] = info["longName"]
+        response[ticker_key] = response_info
+
+    return response
+
+@app.get("/close/{symbol}")
+def get_close(symbol: str, interval: Optional[str] = "1d", start_date: Optional[str] = ""):
     try:
         datetime.strptime(start_date, '%Y-%m-%d')
     except ValueError:
@@ -25,16 +42,8 @@ def get_quote_data(quote: str, interval: str = "1d", start_date: str = ""):
         interval = "1d"
 
     response = {}
-    for (ticker_key, ticker) in yfinance.Tickers(quote).tickers.items():
-        response[ticker_key] = {}
+    for (ticker_key, ticker) in yfinance.Tickers(symbol).tickers.items():
         close = ticker.history(start=start_date, end=end_date, interval=interval)["Close"]
-        response[ticker_key]["close"] = close.to_dict()
-        info = ticker.info
-        response_info = {}
-        response_info["logo_url"] = info["logo_url"]
-        response_info["previous_close"] = info["previousClose"]
-        response_info["short_name"] = info["shortName"]
-        response_info["long_name"] = info["longName"]
-        response[ticker_key]["info"] = response_info
+        response[ticker_key] = close.to_dict()
 
     return response
